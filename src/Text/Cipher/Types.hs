78c1750cfe5dfc55d0df2997ac310770f3d058cc
kmein <kieran.meinhardt@gmail.com>
2016-07-17 12:29:19 +0200

    Add haddock for Text.Cipher.Types

--- src/Text/Cipher/Types.hs
+++ src/Text/Cipher/Types.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE EmptyDataDecls #-}
+-- | This module contains some type synonyms to improve type readability.
 module Text.Cipher.Types where
 
+-- | A phantom type for plaintext.
 data Plain
+
+-- | A phantom type for ciphertext.
 data Cipher
 
+-- | An encrypted or decrypted message.
 type Message a = String
 
+-- | A password / key for ciphers like Vigenère.
 type Key = String
